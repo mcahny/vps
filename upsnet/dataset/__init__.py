4805e891767d0b94c7811a6b2ff7582e2481c0dd
mcahny <mcahny01@gmail.com>
2020-04-24 12:18:31 +0900

    city fuse from pfpn coco

--- upsnet/dataset/__init__.py
+++
@@ -1,5 +0,0 @@
-from .cityscapes import Cityscapes
-from .cityscapes_ext import CityscapesExt
-from .viper import Viper
-from .coco import coco
-from .ade20k import ade20k
--- upsnet/models/__init__.py
+++
@@ -1,1 +0,0 @@
-from .resnet_upsnet import resnet_50_upsnet, resnet_101_upsnet

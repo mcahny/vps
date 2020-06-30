9cf541ca6cdf55202e69426b673d77128159594d
mcahny <mcahny01@gmail.com>
2020-06-30 16:20:58 +0900

    dataset

--- tools/dataset/cityscapes_vps.py
+++ tools/dataset/cityscapes_vps.py
@@ -1,17 +1,3 @@
-# ---------------------------------------------------------------------------
-# Unified Panoptic Segmentation Network
-#
-# Copyright (c) 2018-2019 Uber Technologies, Inc.
-#
-# Licensed under the Uber Non-Commercial License (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at the root directory of this project. 
-#
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# 
-# Written by Yuwen Xiong
-# -----------------------------------------------------------------------
 from __future__ import print_function
 
 import os
